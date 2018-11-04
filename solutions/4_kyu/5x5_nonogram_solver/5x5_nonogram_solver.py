2bb1289484e5336455790de5cc7cf1a40b0b9e9c
Artemii Hrynevych <hrynevychartemii@gmail.com>
2018-11-04 20:44:58 +0100

    Started the 5x5 nonogram solver

---
+++ solutions/4_kyu/5x5_nonogram_solver/5x5_nonogram_solver.py
@@ -0,0 +1,21 @@
+""" Hrynevych Artemii
+My solution for the Codewars "5x5 Nonogram Solver" kata
+https://www.codewars.com/kata/5x5-nonogram-solver/python
+"""
+
+class Nonogram:
+""" Implementation of 5x5 nonogram solver."""
+    def __init__(self, clues):
+        """ Writing downt the clues.
+        
+        Arguments:
+        clues -- tuple of (horizontal clues, vertical clues).
+        Horizontal and vertical clues are given as tuples of indiviadual clues.
+        """
+        pass
+
+    def solve(self):
+        """ Method to solve the nonogram.
+        Returns solution as tuple of rows.
+        """
+        pass
